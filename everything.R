031b7e12f77e4c8d6c6afa84dcbb32009178bac9
Anmay Gupta <anmaygupta1@gmail.com>
2021-08-09 19:29:34 -0500

    Plan created

---
+++ everything.R
@@ -0,0 +1,9 @@
+# Horrendously formatted and buggy code
+
+# First exercise is to break this up into the different files.
+
+# Then we debug it
+
+# And finally we style according to style guide.
+
+read_csv("file.csv")
